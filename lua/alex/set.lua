5f1fd855e30c843d4f7d504065d9c33a4d9605d6
alexborja <gersonperezgerder@gmail.com>
2024-02-04 00:37:36 +0100

    set updated

--- lua/alex/set.lua
+++ lua/alex/set.lua
@@ -4,4 +4,14 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 3
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
